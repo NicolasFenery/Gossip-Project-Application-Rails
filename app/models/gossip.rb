45bbdcb7f027a980bbb53fa4750f27d9bded5797
NicolasFenery <fenery.nicolas@gmail.com>
2019-02-05 16:28:31 +0100

    Bonjour Michelita

--- app/models/gossip.rb
+++ app/models/gossip.rb
@@ -1,4 +1,11 @@
 class Gossip < ApplicationRecord
   belongs_to :user
   has_many :tags
+
+  validates :title, 
+  presence: true, 
+  length: { minimum: 3, maximum: 14}
+
+  validates :content, 
+  presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+
+  resources :gossips
+
   root 'home#index'
-  get 'gossips/:id', to: 'gossips#card', as: 'gossip'
+  get 'gossips/:id', to: 'gossips#card'
   get 'users/:id', to: 'users#profile', as: 'user'
   get 'welcome/:first_name', to: 'welcome#landing'
   get 'pages/team', to: 'pages#team'
