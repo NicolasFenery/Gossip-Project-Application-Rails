0237ba6cb21fcd8820b87e70013373afde44a53e
NicolasFenery <fenery.nicolas@gmail.com>
2019-02-06 11:37:30 +0100

    Sup Bruhhhhhhh

--- app/controllers/gossips_controller.rb
+++ app/controllers/gossips_controller.rb
@@ -1,5 +1,21 @@
 class GossipsController < ApplicationController
-  def card
+
+  def new
+    @gossip = Gossip.new
+  end
+
+  def show
     @gossip_card = Gossip.find(params[:id])
   end
+
+  def create
+    @gossip = Gossip.create(title: params[:title], content: params[:content], user_id: User.all.sample.id)
+
+    if @gossip.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,8 @@ Rails.application.routes.draw do
   resources :gossips
 
   root 'home#index'
-  get 'gossips/:id', to: 'gossips#card'
-  get 'users/:id', to: 'users#profile', as: 'user'
   get 'welcome/:first_name', to: 'welcome#landing'
+  get 'users/:id', to: 'users#profile', as: 'user'
   get 'pages/team', to: 'pages#team'
   get 'pages/contact', to: 'pages#contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
